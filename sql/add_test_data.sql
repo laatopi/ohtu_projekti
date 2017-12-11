b6e08a12a28732a072e5bf2a5115e3226fe8eead
Sami Lindqvist <sami.lindqvist@helsinki.fi>
2017-12-11 21:36:54 +0200

    test data

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,6 +2,12 @@
 INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('The Bee-pocalypse', 'Gimlet Media', null, 'https://gimletmedia.com/episode/the-bee-pocalypse/', 'podcast', null, '02.11.2017', 'Science Vs');
 INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('Should all locks have keys?', 'CGP Grey', null, 'https://www.youtube.com/watch?v=VPBH1eW28mo', 'video', null, '14.04.2016', null);
 INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('The Agile Samurai', 'Jonathan Rasmusson','9781934356586', null, 'kirja', 'The Agile Samurai gives you everything you need to deliver something of value every week and make rolling your software into production a non-event.', '2010', null);
+INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('The Lord of The Rings', 'J.R.R Tolkien','9788373191723', null, 'kirja', "The Lord of the Rings is an epic high fantasy novel written by English author and scholar J. R. R. Tolkien. The story began as a sequel to Tolkien's 1937 fantasy novel The Hobbit, but eventually developed into a much larger work.", '1954', null);
+INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('Mielensäpahoittajan Suomi', 'Tuomas Kyrö','9789510421109', null, 'kirja', "Sata itsenäisen Suomen vuotta, sata Mielensäpahoittajan tarinaa. Yksi suuri kertomus. Kansakunta loikkasi suolta sushibaariin hetkessä eikä haittaa, vaikka toinen saapas on suossa yhä. Mielensäpahoittajan silmin näemme maatalousvaltaisen ja kännykkävaltaisen Suomen, miesvaltaisen ja naisvaltaisen, ainakin kohtalaisen.", '2017', null);
+INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('Map of Computer Science', 'Domain of Science', null, 'https://www.youtube.com/watch?v=SzJ46YA_RaA', 'video', null, '06.10.2017', null);
+INSERT INTO Lukuvinkki(otsikko, tekija, isbn, url, tyyppi, kuvaus, julkaistu, sarja) VALUES('Should You Learn C++ in 2018?', 'Stefan Mischook', null, 'https://www.youtube.com/watch?v=7xVrYnyQ04M', 'video', null, '13.11.2017', null);
+
+
 INSERT INTO Tag(nimi) VALUES ('tietojenkäsittely');
 INSERT INTO Tag(nimi) VALUES ('tietoverkot');
 INSERT INTO Tag(nimi) VALUES ('agile');
